0655f78513d2a890702efd35ac97ce0b1a88e20d
Qi, Chengri <chengri.a.qi@rakuten.com>
2019-12-30 16:58:02 +0900

    node

--- README.md
+++ README.md
@@ -4,14 +4,18 @@ git clone https://github.com/im-bravo/mariadb-galera-cluster.git
 cd mariadb-galera-cluster
 
 export MARIADB_SELF_NODE="simba-db-01"
-export MARIADB_ALL_NODES="simba-db-01,simba-db-02,simba-db-03"
+export NODE1_HOST="simba-db-01"
+export NODE2_HOST="simba-db-02"
+export NODE3_HOST="simba-db-03"
 sh update-config.sh
 
 # first
 galera_new_cluster
+systemctl status mariadb
 
 # second-
 systemctl start mariadb
+systemctl status mariadb
 
 mysql
 show status like 'wsrep_%';
--- update-config.sh
+++ update-config.sh
@@ -1,6 +1,9 @@
+get_internal_ip_address() {
+    nslookup $1 | grep Address | tail -1 | cut -d:  -f2 | sed 's/^ *\| *$//'
+}
+
 my_internal_ip_address() {
-    # /sbin/ip -o -4 addr list ens4 | awk '{print $4}' | cut -d/ -f1
-    hostname -I
+    /sbin/ip -o -4 addr list ens4 | awk '{print $4}' | cut -d/ -f1
 }
 
 create_config_file() {
@@ -12,8 +15,15 @@ create_config_file() {
 export MARIADB_SELF_NODE_NAME=${MARIADB_SELF_NODE}
 export MARIADB_SELF_NODE_ADDRESS=`hostname -I | tr -d ' '`
 
+export NODE1_INTERNAL_IP=`get_internal_ip_address $NODE1_HOST`
+export NODE2_INTERNAL_IP=`get_internal_ip_address $NODE2_HOST`
+export NODE3_INTERNAL_IP=`get_internal_ip_address $NODE3_HOST`
+export MARIADB_ALL_NODES="$NODE1_INTERNAL_IP,$NODE2_INTERNAL_IP,$NODE3_INTERNAL_IP"
+
+
 echo "MARIADB_SELF_NODE_NAME:$MARIADB_SELF_NODE_NAME"
 echo "MARIADB_SELF_NODE_ADDRESS:$MARIADB_SELF_NODE_ADDRESS"
+echo "MARIADB_ALL_NODES:$MARIADB_ALL_NODES"
 
 
 # backup
