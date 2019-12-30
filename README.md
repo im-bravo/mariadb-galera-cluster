edcce73df4989a71922baca09a7a5a90ba609b95
Qi, Chengri <chengri.a.qi@rakuten.com>
2019-12-30 16:40:31 +0900

    dd

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mariadb-galera-cluster
 # mariadb-galera-cluster
+git clone https://github.com/im-bravo/mariadb-galera-cluster.git
+cd mariadb-galera-cluster
+
+export MARIADB_SELF_NODE="simba-db-01"
+export MARIADB_ALL_NODES="simba-db-01,simba-db-02,simba-db-03"
+sh update-config.sh
+
+# first
+galera_new_cluster
+
+# second-
+systemctl start mariadb
+
+mysql
+show status like 'wsrep_%';
--- update-config.sh
+++ update-config.sh
@@ -1,10 +1,16 @@
-
+my_internal_ip_address() {
+    # /sbin/ip -o -4 addr list ens4 | awk '{print $4}' | cut -d/ -f1
+    hostname -I
+}
 
 create_config_file() {
     # edit
-    cat template.server.cnf | sed "s/MARIADB_SELF_NODE/$MARIADB_SELF_NODE/g" | sed "s/MARIADB_ALL_NODES/$MARIADB_ALL_NODES/g" > server.cnf
+    cat template.server.cnf | sed "s/MARIADB_SELF_NODE_NAME/$MARIADB_SELF_NODE_NAME/g" | sed "s/MARIADB_SELF_NODE_ADDRESS/$MARIADB_SELF_NODE_ADDRESS/g" | sed "s/MARIADB_ALL_NODES/$MARIADB_ALL_NODES/g" > server.cnf
     diff template.server.cnf server.cnf
 }
+
+export MARIADB_SELF_NODE_NAME=${MARIADB_SELF_NODE}
+export MARIADB_SELF_NODE_ADDRESS=`hostname -I`
 
 
 # backup
@@ -12,3 +18,7 @@ cp /etc/my.cnf.d/server.cnf /etc/my.cnf.d/server.cnf.bk02
 create_config_file
 # replace
 cp server.cnf /etc/my.cnf.d/server.cnf
+
+
+
+
